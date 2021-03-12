40264182a3c27e2bf1e85a4fd4764b7b07a77c55
Ricky Rozario <rickyfrancisrozario@gmail.com>
2021-03-12 16:32:45 +0600

    Fetching Products From backend using axios and  React (useEffect)

--- backend/server.js
+++ backend/server.js
@@ -9,12 +9,12 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/products', (req, res) => {
-  res.json({ products });
+  res.json(products);
 });
 
 app.get('/api/product/:id', (req, res) => {
   const product = products.find((product) => product._id === req.params.id);
-  res.json({ product });
+  res.json(product);
 });
 
 app.listen(5000, console.log('Server running on port 5000'));
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
+
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../components/Rating';
-import products from '../products';
+// import products from '../products';
 
 const ProductScreen = ({ match }) => {
-  const product = products.find((product) => product._id === match.params.id);
+  // const product = products.find((product) => product._id === match.params.id);
+
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/product/${match.params.id}`);
+      setProduct(data);
+    };
+
+    fetchProduct();
+  }, []);
+
   const {
     name,
     rating,
@@ -30,7 +44,9 @@ const ProductScreen = ({ match }) => {
               <h2>{name}</h2>
             </ListGroup.Item>
             <ListGroup.Item>
-              <Rating value={rating} text={`${numReviews} reviews`} />
+              {rating && (
+                <Rating value={rating} text={`${numReviews} reviews`} />
+              )}
             </ListGroup.Item>
             <ListGroup.Item>Price: ${price}</ListGroup.Item>
             <ListGroup.Item>Description: {description}</ListGroup.Item>
