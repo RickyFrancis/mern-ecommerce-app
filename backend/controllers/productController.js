779df67f4774e4445a9d2b789208eaaa4f785a84
Ricky Rozario <rickyfrancisrozario@gmail.com>
2021-03-28 11:02:35 +0600

    Implemented: 1. User Register Reducer, Action & Screen // 2. Update Profile Endpoint // 3. Profile Screen & Get User Details // 4. Update User Profile

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,6 @@
 import asyncHandler from 'express-async-handler';
 import Product from '../models/productModel.js';
+import mongoose from 'mongoose';
 
 // @desc Fetch all products
 // @route GET /api/products
